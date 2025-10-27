ff3597f42448691e3b8213b9cb16bd8882275237
Gustas <gustasjucaitis@gmail.com>
2025-10-27 13:05:43 +0100

    Uploading results as well

--- test_Yolo/trainModel.py
+++ test_Yolo/trainModel.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo12m.pt")
 
 # Train the model on Construction-PPE dataset
-model.train(data="dataset/data.yaml", epochs=100, imgsz=640, save=True)
+model.train(data="dataset/data.yaml", epochs=100, imgsz=640)
